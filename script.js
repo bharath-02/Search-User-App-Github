e9eda65da32fb9a0b7eba8f051dc136c12c4385c
bhara <bharathJ222000@gmail.com>
2020-10-30 10:38:37 +0530

    started

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ form.addEventListener('submit', async function(submit) {
     var response = await fetch('https://api.github.com/users/' + name);
     var data = await response.json();
     console.log(data);
+
+    var result = document.getElementById('resultData').innerHTML = `
+        <a href=${data.repos_url} target="_blank"> Repositories </a>
+    `;
+    // var a = document.createElement('a');
+    // a.setAttribute('href', data.repos_url);
+    // a.setAttribute('target', '_blank')
+    // a.innerHTML = 'Repos';
+    // result.append(a);
+    // result.innerHTML = ;
 })
