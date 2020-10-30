bf35fd7ef8616c79a2812a062168298b5ce9acf6
bhara <bharathJ222000@gmail.com>
2020-10-30 09:57:47 +0530

    started

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var form = document.getElementById('myForm');
 
-form.addEventListener('submit', function(submit) {
-    submit.preventDefault();
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
 
-
+    var search = document.getElementById('search').value;
+    alert(search);
 })
