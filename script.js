ee9bea3ef10b92f14584218bcb6080b0846ad84f
bhara <bharathJ222000@gmail.com>
2020-10-31 07:43:55 +0530

    started

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// ID of the form element
 var form = document.getElementById('myForm');
 
+// Adding Event Listener while the form gets submmitted
 form.addEventListener('submit', async function(submit) {
+    // To prevent refreshing add prevent default
     submit.preventDefault();
 
+    // Get the username entered in the text-box
     var name = document.getElementById('search').value;
 
+    // Fetch the data using the below API
     var response = await fetch('https://api.github.com/users/' + name);
     var data = await response.json();
     console.log(data);
 
-    var result = document.getElementById('resultData');
+    var result = document.getElementById('repoData');
 
-    document.getElementById('repoData').innerHTML = '';
+    result.innerHTML = '';
 
     var a = document.createElement('a');
     a.setAttribute('href', '#');
@@ -20,11 +25,14 @@ form.addEventListener('submit', async function(submit) {
     a.innerHTML = 'View Repositories';
     result.append(a);
 
-    a.addEventListener('click', async function() {
+    a.addEventListener('click', async function(s) {
+        var value = prompt('How many repositories per page');
+        s.preventDefault();
         document.getElementById('repoid').style.display = 'none';
         var reposData = await fetch('https://api.github.com/users/' + name + '/repos');
         var repo = await reposData.json();
         console.log(repo);
+
         var repoResult = document.getElementById('repoData');
         for (var i = 0; i < data.public_repos; i++) {
             var div = document.createElement('div');
